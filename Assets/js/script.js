8d9b5fb72054766b1be6e3b1fb39e239271d56b9
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2022-10-28 19:11:01 -0700

    js code for 5 day weather

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,10 @@
 var key = "8191da8a9c456bb383721b6d870ef05d"
 var searchBtn = document.querySelector("#search-btn");
 var currentWeather = document.querySelector("#current-weather");
+var fiveDayEl = document.querySelector("#fiveDay");
+var windEl = document.querySelector(".wind")
+var humidityEl = document.querySelector(".humidity")
+
 function getWeather (cityName) {
     var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=imperial`
     fetch(apiUrl)
@@ -15,9 +19,26 @@ function getWeather (cityName) {
         fetch(url)
         .then(response => response.json())
         .then(fiveDay => {
-            console.log(fiveDay);
+            console.log(fiveDay.list.length)
+            for( var i = 3; i < fiveDay.list.length; i+=8){
+                console.log(fiveDay.list[i])
+                var card = document.createElement("div")
+                card.classList.add("col-sm-2")
+                var content = `
+                <div class="card border-dark">
+                    <div id="dayOne" class="card-body">
+                        <h5 class="card-title">${fiveDay.list[i].dt_txt}</h5>
+                        <p class="temp">Temp: ${fiveDay.list[i].main.temp}</p>
+                        <p class="wind">Wind: ${fiveDay.list[i].wind.speed}</p>
+                        <p class="humidity">Humidity: ${fiveDay.list[i].main.humidity}</p>
+                    </div>
+                </div>`
+                card.innerHTML = content
+                fiveDayEl.appendChild(card)
+                // console.log(fiveDay)
+            }
+            
         })
-        // dayOne.innerHTML = 
        })
 } 
 searchBtn.addEventListener("click", (event) => {
